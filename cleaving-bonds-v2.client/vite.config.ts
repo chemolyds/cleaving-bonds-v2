5af86db91a5ec50491015478a977bdd01f36e3b2
Jerdan1980 <jeremytinana@gmail.com>
2024-05-03 15:38:44 -0400

    Add api to server proxy

--- cleaving-bonds-v2.client/vite.config.ts
+++ cleaving-bonds-v2.client/vite.config.ts
@@ -57,6 +57,10 @@ export default defineConfig({
         target,
         secure: false,
       },
+      '/api': {
+        target,
+        secure: false,
+      },
     },
     port: 5173,
     https: {
