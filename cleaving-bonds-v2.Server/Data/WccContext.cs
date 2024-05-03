0a42284929114727f7ddd1ad65c5ac9111309e72
Jerdan1980 <jeremytinana@gmail.com>
2024-05-03 14:25:51 -0400

    Install EF Core w/ SQL

---
+++ cleaving-bonds-v2.Server/Data/WccContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace cleaving_bonds_v2.Server.Data
+{
+    public class WccContext : DbContext
+    {
+        public WccContext(DbContextOptions<WccContext> options) : base(options)
+        {
+        }
+    }
+}
