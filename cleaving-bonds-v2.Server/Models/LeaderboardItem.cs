581e16b22f1b7248cc9225cb0356be8992903a59
Jerdan1980 <jeremytinana@gmail.com>
2024-05-03 14:26:31 -0400

    Create leaderboard models

---
+++ cleaving-bonds-v2.Server/Models/LeaderboardItem.cs
@@ -0,0 +1,17 @@
+﻿namespace cleaving_bonds_v2.Server.Models
+{
+    public class LeaderboardItem
+    {
+        public string Name { get; set; }
+        public float BronzeScore { get; set; } = 0;
+        public float SilverScore { get; set; } = 0;
+        public float GoldScore { get; set; } = 0;
+
+        public float TotalScore
+        {
+            get {
+                return BronzeScore + SilverScore + GoldScore;
+            }
+        }
+    }
+}
---
+++ cleaving-bonds-v2.Server/Models/Tier.cs
@@ -0,0 +1,7 @@
+﻿namespace cleaving_bonds_v2.Server.Models
+{
+    public enum Tier
+    {
+        Bronze, Silver, Gold
+    }
+}
