1a96fd7865118ec628cda19ed037a151f700583a
Jerdan1980 <jeremytinana@gmail.com>
2024-05-03 15:39:04 -0400

    Create leaderbard page

---
+++ cleaving-bonds-v2.client/src/components/TeamScore.tsx
@@ -0,0 +1,67 @@
+import { Card, ProgressBar } from 'react-bootstrap'
+
+export default function TeamScore({
+  name,
+  bronzeScore = 0,
+  silverScore = 0,
+  goldScore = 0,
+  totalScore = 0,
+  placement,
+}: {
+  name: string
+  bronzeScore?: number
+  silverScore?: number
+  goldScore?: number
+  totalScore?: number
+  placement: number
+}) {
+  const cardStyle = (
+    <Card className="">
+      {placement <= 3 && (
+        <Card.Header className={`card-${rankName(placement)}`}>
+          {rankName(placement)}
+        </Card.Header>
+      )}
+      <Card.Body>
+        <div className="d-flex justify-content-between">
+          <h4>{name}</h4>
+          <h4>{totalScore.toFixed(1)}</h4>
+        </div>
+        <ProgressBar>
+          <ProgressBar now={bronzeScore} className="progress-bronze" />
+          <ProgressBar now={silverScore} className="progress-silver" />
+          <ProgressBar now={goldScore} className="progress-gold" />
+        </ProgressBar>
+      </Card.Body>
+    </Card>
+  )
+
+  const divStyle = (
+    <div>
+      <div className="d-flex justify-content-between">
+        <h4>{name}</h4>
+        <h4>{totalScore.toFixed(1)}</h4>
+      </div>
+      <ProgressBar>
+        <ProgressBar now={bronzeScore} variant="success" max={112} />
+        <ProgressBar now={silverScore} variant="warning" max={112} />
+        <ProgressBar now={goldScore} variant="danger" max={112} />
+      </ProgressBar>
+    </div>
+  )
+
+  return cardStyle
+}
+
+const rankName = (rank: number) => {
+  switch (rank) {
+    case 1:
+      return '1st'
+    case 2:
+      return '2nd'
+    case 3:
+      return '3rd'
+    default:
+      return `${rank}th`
+  }
+}
--- cleaving-bonds-v2.client/src/pages/Leaderboard.tsx
+++ cleaving-bonds-v2.client/src/pages/Leaderboard.tsx
@@ -1,11 +1,67 @@
+import { Stack } from 'react-bootstrap'
+import useSWR from 'swr'
+import TeamScore from '../components/TeamScore'
+import { fetcher } from '../components/fetcher'
+
 export default function Leaderboard() {
+  const { data: bronzeTeams } = useSWR('/api/Leaderboard/bronze', fetcher)
+  const { data: silverTeams } = useSWR('/api/Leaderboard/silver', fetcher)
+  const { data: goldTeams } = useSWR('/api/Leaderboard/gold', fetcher)
+
+  console.log(bronzeTeams)
+
   return (
     <>
       <h1>Leaderboard</h1>
       <div className="row">
-        <div className="col">One of three columns</div>
-        <div className="col">One of three columns</div>
-        <div className="col">One of three columns</div>
+        <div className="col">
+          <h1>Bronze</h1>
+          <Stack gap={3}>
+            {bronzeTeams?.map((team, index) => (
+              <TeamScore
+                key={team.name}
+                name={team.name}
+                bronzeScore={team.bronzeScore}
+                silverScore={team.silverScore}
+                goldScore={team.goldScore}
+                totalScore={team.totalScore}
+                placement={index + 1}
+              />
+            ))}
+          </Stack>
+        </div>
+        <div className="col">
+          <h1>Silver</h1>
+          <Stack gap={3}>
+            {silverTeams?.map((team, index) => (
+              <TeamScore
+                key={team.name}
+                name={team.name}
+                bronzeScore={team.bronzeScore}
+                silverScore={team.silverScore}
+                goldScore={team.goldScore}
+                totalScore={team.totalScore}
+                placement={index + 1}
+              />
+            ))}
+          </Stack>
+        </div>
+        <div className="col">
+          <h1>Gold</h1>
+          <Stack gap={3}>
+            {goldTeams?.map((team, index) => (
+              <TeamScore
+                key={team.name}
+                name={team.name}
+                bronzeScore={team.bronzeScore}
+                silverScore={team.silverScore}
+                goldScore={team.goldScore}
+                totalScore={team.totalScore}
+                placement={index + 1}
+              />
+            ))}
+          </Stack>
+        </div>
       </div>
     </>
   )
