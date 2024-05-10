f408b3b89fe4a41cf96e5eb441368c69dbecb4f9
Jerdan1980 <jeremytinana@gmail.com>
2024-05-10 15:18:25 -0400

    Create Logout Procedure

---
+++ cleaving-bonds-v2.Server/Controllers/IdentityController.cs
@@ -0,0 +1,32 @@
+﻿using cleaving_bonds_v2.Server.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace cleaving_bonds_v2.Server.Controllers
+{
+    //[Route("api/[controller]")]
+    [ApiController]
+    public class IdentityController : ControllerBase
+    {
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public IdentityController(SignInManager<ApplicationUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        [HttpPost("/logout")]
+        [Authorize]
+        public async Task<IResult> Logout(object empty)
+        {
+            if (empty != null)
+            {
+                await _signInManager.SignOutAsync();
+                return Results.Ok();
+            }
+            return Results.Unauthorized();
+        }
+    }
+}
