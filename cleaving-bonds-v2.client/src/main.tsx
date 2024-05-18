2690b352fc7af5e39aad09299fb76afb6a64b8db
Jerdan1980 <jeremytinana@gmail.com>
2024-05-18 02:14:32 -0400

    Implement PDF displayer

--- cleaving-bonds-v2.client/src/main.tsx
+++ cleaving-bonds-v2.client/src/main.tsx
@@ -1,15 +1,19 @@
 import '@/assets/styles.scss'
 import Home from '@/pages/Home'
 import Leaderboard from '@/pages/Leaderboard'
+import Login from '@/pages/Login'
+import Logout from '@/pages/Logout'
+import Question from '@/pages/Question'
+import QuestionSetsList from '@/pages/QuestionSetsList'
+import AdminDashboard from '@/pages/admin/Dashboard'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { pdfjs } from 'react-pdf'
+import 'react-pdf/dist/esm/Page/AnnotationLayer.css'
+import 'react-pdf/dist/esm/Page/TextLayer.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import App from './App'
-import Login from './pages/Login'
-import Logout from './pages/Logout'
-import QuestionSetsList from './pages/QuestionSetsList'
-import AdminDashboard from './pages/admin/Dashboard'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
@@ -21,6 +25,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
           <Route path="login" element={<Login />} />
           <Route path="logout" element={<Logout />} />
           <Route path="sets" element={<QuestionSetsList />} />
+          <Route path="set/:tier/:topic" element={<Question />} />
           <Route path="admin">
             <Route path="dashboard" element={<AdminDashboard />} />
           </Route>
@@ -29,3 +34,5 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     </BrowserRouter>
   </React.StrictMode>,
 )
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`
