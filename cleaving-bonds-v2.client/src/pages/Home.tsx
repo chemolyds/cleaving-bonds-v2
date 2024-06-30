565ae736cef96a1574333e81195cab6b693e819d
Jerdan1980 <jeremytinana@gmail.com>
2024-06-29 20:39:42 -0400

    Remove tutorial weatherforecast route

--- cleaving-bonds-v2.client/src/pages/Home.tsx
+++ cleaving-bonds-v2.client/src/pages/Home.tsx
@@ -1,55 +1,7 @@
 import { ReferenceType, references } from '@/assets/references'
-import { useEffect, useState } from 'react'
 import { Button } from 'react-bootstrap'
 
-interface Forecast {
-  date: string
-  temperatureC: number
-  temperatureF: number
-  summary: string
-}
-
 export default function Home() {
-  const [forecasts, setForecasts] = useState<Forecast[]>()
-
-  useEffect(() => {
-    populateWeatherData()
-  }, [])
-
-  const contents =
-    forecasts === undefined ? (
-      <p>
-        <em>
-          Loading... Please refresh once the ASP.NET backend has started. See{' '}
-          <a href="https://aka.ms/jspsintegrationreact">
-            https://aka.ms/jspsintegrationreact
-          </a>{' '}
-          for more details.
-        </em>
-      </p>
-    ) : (
-      <table className="table table-striped" aria-labelledby="tableLabel">
-        <thead>
-          <tr>
-            <th>Date</th>
-            <th>Temp. (C)</th>
-            <th>Temp. (F)</th>
-            <th>Summary</th>
-          </tr>
-        </thead>
-        <tbody>
-          {forecasts.map((forecast) => (
-            <tr key={forecast.date}>
-              <td>{forecast.date}</td>
-              <td>{forecast.temperatureC}</td>
-              <td>{forecast.temperatureF}</td>
-              <td>{forecast.summary}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    )
-
   return (
     <>
       <h1>Welcome to WCC!</h1>
@@ -57,17 +9,6 @@ export default function Home() {
       {references.map((ref: ReferenceType) => (
         <Button href={ref.href}>{ref.title}</Button>
       ))}
-
-      <h1 id="tableLabel">Weather forecast</h1>
-      <p>This component demonstrates fetching data from the server.</p>
-      <Button variant="outline-danger">tests bootstrap</Button>
-      {contents}
     </>
   )
-
-  async function populateWeatherData() {
-    const response = await fetch('weatherforecast')
-    const data = await response.json()
-    setForecasts(data)
-  }
 }
